6ea9c214fbd45795426a941be2db4843cd4c1535
pablomitchell <pablo.mitchell@gmail.com>
2021-02-04 10:19:04 -0800

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup
 setup(
     name='haba',
     version='0.1',
-    packages=['haba'],
+    packages=[
+        'haba',
+        'haba.differencing',
+        'haba.labeling',
+        'haba.util',
+    ],
     url='https://github.com/pablomitchell/haba',
-    license='GPL',
+    license='License?',
     author='Pablo Mitchell',
     author_email='pablo.mitchell@gmail.com',
     description='Machine Learning Framework for Finance'
