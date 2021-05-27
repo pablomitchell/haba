effa63121e5609f80dfa1a626190efddb7811e1e
Pablo Mitchell <pablo.mitchell@gmail.com>
2021-05-27 06:45:09 -0700

    Renamed normalize to standardize

--- haba/util/stats.py
+++ haba/util/stats.py
@@ -7,52 +7,6 @@ from numba import njit, prange
 from scipy import stats as sps
 
 
-def normalize(arr, zmax=6, zerr=9, method='uniform'):
-    """
-    First computes robust standard deviation (rstd), sets any
-    data exceeding
-
-        +/- zerr * rstd
-
-    to NaN, and then clips data inside
-
-        +/- zmax * rstd
-
-    Finally, the data is normalized using 'method'
-
-    Parameters
-    ----------
-    arr : numpy array or pandas object
-        object holding data to be normalized
-    zmax: int, default 6
-        z-score value used to clip data
-    zerr: int, default 9
-        z-score value used to identify errors in data
-    method : str, default 'uniform'
-        method used to normalize data:  ('uniform', 'normal')
-
-    Returns
-    -------
-    normalized : numpy array
-
-    """
-    rstd = 1.4826 * np.nanmedian(np.abs(arr - np.nanmedian(arr, axis=0)), axis=0)
-    cleaned = np.where(np.abs(arr) < zerr * rstd, arr, np.full_like(arr, np.nan))
-    clipped = np.clip(cleaned, -zmax * rstd, +zmax * rstd)
-
-    if method == 'uniform':
-        smallest = np.nanmin(clipped, axis=0)
-        biggest = np.nanmax(clipped, axis=0)
-        return (clipped - smallest) / (biggest - smallest)
-    elif method == 'normal':
-        mean = np.nanmean(clipped, axis=0)
-        std = np.nanstd(clipped, axis=0)
-        return (clipped - mean) / std
-    else:
-        msg = f'Invalid method: {method}'
-        raise Exception(msg)
-
-
 @njit
 def _resample(arr, n_resamples):
     counter = 0
@@ -80,16 +34,49 @@ def resample(seq, accuracy=0.1, confidence=0.05):
     return _resample(seq, n_resamples)
 
 
+def standardize(arr, zmax=6, zerr=9, method='uniform'):
+    """
+    Standardize data
 
-if __name__ == '__main__':
-    import pandas as pd
+    First computes robust standard deviation (rstd), sets any
+    data exceeding
 
-    obj = pd.DataFrame(np.random.randn(20, 3), columns=list('abc'))
-    obj.loc[10, 'a'] = 10
-    # obj = pd.Series(np.random.randn(20))
-    # obj.loc[10] = 10
-    obj_normed = normalize(obj, 6, 9, method='normal')
+        +/- zerr * rstd
 
-    print(obj.to_string())
-    print('-'*45)
-    print(obj_normed)
+    to NaN and then clips data inside
+
+        +/- zmax * rstd
+
+    Finally, the data is standardized using 'method'
+
+    Parameters
+    ----------
+    arr : numpy array or pandas object
+        object holding data to be normalized
+    zmax: int, default 6
+        z-score value used to clip data
+    zerr: int, default 9
+        z-score value used to identify errors in data
+    method : str, default 'uniform'
+        method used to normalize data:  ('uniform', 'normal')
+
+    Returns
+    -------
+    standardized : numpy array
+
+    """
+    rstd = 1.4826 * np.nanmedian(np.abs(arr - np.nanmedian(arr, axis=0)), axis=0)
+    cleaned = np.where(np.abs(arr) < zerr * rstd, arr, np.full_like(arr, np.nan))
+    clipped = np.clip(cleaned, -zmax * rstd, +zmax * rstd)
+
+    if method == 'uniform':
+        smallest = np.nanmin(clipped, axis=0)
+        biggest = np.nanmax(clipped, axis=0)
+        return (clipped - smallest) / (biggest - smallest)
+    elif method == 'normal':
+        mean = np.nanmean(clipped, axis=0)
+        std = np.nanstd(clipped, axis=0)
+        return (clipped - mean) / std
+    else:
+        msg = f'Invalid method: {method}'
+        raise Exception(msg)
