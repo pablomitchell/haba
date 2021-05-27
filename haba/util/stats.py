faf19032c25afc24bdeae11cbbd54f3185fe244b
Pablo Mitchell <pablo.mitchell@gmail.com>
2021-05-27 06:41:18 -0700

    Implemented util.stats.normalize logic

--- haba/util/stats.py
+++ haba/util/stats.py
@@ -7,6 +7,52 @@ from numba import njit, prange
 from scipy import stats as sps
 
 
+def normalize(arr, zmax=6, zerr=9, method='uniform'):
+    """
+    First computes robust standard deviation (rstd), sets any
+    data exceeding
+
+        +/- zerr * rstd
+
+    to NaN, and then clips data inside
+
+        +/- zmax * rstd
+
+    Finally, the data is normalized using 'method'
+
+    Parameters
+    ----------
+    arr : numpy array or pandas object
+        object holding data to be normalized
+    zmax: int, default 6
+        z-score value used to clip data
+    zerr: int, default 9
+        z-score value used to identify errors in data
+    method : str, default 'uniform'
+        method used to normalize data:  ('uniform', 'normal')
+
+    Returns
+    -------
+    normalized : numpy array
+
+    """
+    rstd = 1.4826 * np.nanmedian(np.abs(arr - np.nanmedian(arr, axis=0)), axis=0)
+    cleaned = np.where(np.abs(arr) < zerr * rstd, arr, np.full_like(arr, np.nan))
+    clipped = np.clip(cleaned, -zmax * rstd, +zmax * rstd)
+
+    if method == 'uniform':
+        smallest = np.nanmin(clipped, axis=0)
+        biggest = np.nanmax(clipped, axis=0)
+        return (clipped - smallest) / (biggest - smallest)
+    elif method == 'normal':
+        mean = np.nanmean(clipped, axis=0)
+        std = np.nanstd(clipped, axis=0)
+        return (clipped - mean) / std
+    else:
+        msg = f'Invalid method: {method}'
+        raise Exception(msg)
+
+
 @njit
 def _resample(arr, n_resamples):
     counter = 0
@@ -36,14 +82,14 @@ def resample(seq, accuracy=0.1, confidence=0.05):
 
 
 if __name__ == '__main__':
-    import time
-    size = 260
+    import pandas as pd
 
-    print('running...')
-    t0 = time.time()
+    obj = pd.DataFrame(np.random.randn(20, 3), columns=list('abc'))
+    obj.loc[10, 'a'] = 10
+    # obj = pd.Series(np.random.randn(20))
+    # obj.loc[10] = 10
+    obj_normed = normalize(obj, 6, 9, method='normal')
 
-    for _ in range(size*20*):
-        resample(np.random.rand(size))
-
-    t1 = time.time()
-    print(f'done after: {t1 - t0:.2f} seconds')
+    print(obj.to_string())
+    print('-'*45)
+    print(obj_normed)
